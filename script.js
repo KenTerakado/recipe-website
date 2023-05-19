05e7b820297aa23d66a743703a3585887ee02409
Ken Terakado <tken915@gmail.com>
2023-05-19 12:42:32 +0900

    PR request changes

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function interpolateColors(startColor, endColor, opacity) {
         }
     return interpolatedColor;
 }
+
+function goToPage() {
+    var selectedOption = document.getElementById("dropdown").value;
+    if (selectedOption !== "") {
+      window.location.href = selectedOption
+    }
+  }
+  function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
